19b4e782db069a486cf81e9c1c72cafc78364fee
Alekshar <alekshar.shadow@gmail.com>
2016-10-19 18:11:45 +0200

    work done

--- src/model/index.js
+++ src/model/index.js
@@ -1,2 +1,71 @@
-
 /* TODO : Créer le modèle objet ici */ 
+
+export class Sensor{
+	constructor(data){
+		this.id = data.id;
+		this.name = data.name;
+		this.data = new Factory().createData(data.data);
+	}
+}
+
+export class Factory{
+	constructor(){
+	}
+	createSensor(data){
+		switch(data.type){
+		case "TEMPERATURE":
+			return new Temperature(data);
+		case "DOOR":
+			return new Door(data);
+		case "FAN_SPEED":
+			return new FanSpeed(data);
+		}
+	}
+
+	createData(data){
+		if(Array.isArray(data.values) && Array.isArray(data.labels)){
+			return new TimeSeries(data);
+		} else {
+			return new Datum(data);
+		}
+	}
+}
+
+export class Temperature extends Sensor{
+	constructor(data){
+		super(data);
+	}
+}
+
+export class Door extends Sensor{
+	constructor(data){
+		super(data);
+	}
+}
+
+export class FanSpeed extends Sensor{
+	constructor(data){
+		super(data);
+	}
+}
+
+
+export class Data{
+	constructor(){
+	}
+}
+
+export class TimeSeries extends Data{
+	constructor(data){
+		super(data);
+		this.values = data.values;
+		this.labels = data.labels;
+	}
+}
+
+export class Datum extends Data{
+	constructor(data){
+		super(data);
+		this.value = data.value;
+	}
+}
--- test/model/index.js
+++ test/model/index.js
@@ -1,10 +1,35 @@
 import expect from 'expect';
 
-// import { ??? } from '../../src/model';
+import { Factory } from '../../src/model';
+import { Sensor } from '../../src/model';
+import { Temperature } from '../../src/model';
+import { FanSpeed } from '../../src/model';
+import { Door } from '../../src/model';
+import { Data } from '../../src/model';
+import { Datum } from '../../src/model';
+import { TimeSeries } from '../../src/model';
 
 import  { data }  from './sensors_data'
 console.log(data.length);
 
 describe('Sensor model tests', () => {
   /* TODO: Ecrire ici la suite de tests pourle modèle objet.*/
+	it("first data should be a temperature sensor with timeseries data type", function(){
+		var factory = new Factory();
+		var temp = factory.createSensor(data[0]);
+		expect(temp instanceof Temperature).toBeTruthy();
+		expect(temp.data instanceof TimeSeries).toBeTruthy();
+	});
+	it("second data should be a door sensor with datum data type", function(){
+		var factory = new Factory();
+		var temp = factory.createSensor(data[1]);
+		expect(temp instanceof Door).toBeTruthy();
+		expect(temp.data instanceof Datum).toBeTruthy();
+	});
+	it("first data should be a fanspeed sensor with timeseries data type", function(){
+		var factory = new Factory();
+		var temp = factory.createSensor(data[2]);
+		expect(temp instanceof FanSpeed).toBeTruthy();
+		expect(temp.data instanceof TimeSeries).toBeTruthy();
+	});
 });
